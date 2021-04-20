8e197fc4099dbadba7758a4f5318f6b21e701902
Vlad Zaharia <vlad@zhr.one>
2021-04-19 23:48:57 -0700

    Fix global interactions

--- themes/vladgg/static/js/home.js
+++ themes/vladgg/static/js/home.js
@@ -1,30 +1,30 @@
 var contentElems = document.getElementsByClassName("grid-iama-content");
-var i = 0;
+var home_i = 0;
 
 setInterval(function() {
     var visibleElems = [];
 
-    for (var j = 0; j < 7; j++) {
-        var idx = (i + j) % contentElems.length;
+    for (var home_j = 0; home_j < 7; home_j++) {
+        var idx = (home_i + home_j) % contentElems.length;
         var elem = contentElems[idx];
 
         elem.classList.remove("hidden");
-        elem.id = "content-" + j;
+        elem.id = "content-" + home_j;
 
         visibleElems.push(idx);
     }
 
-    for (var j = 0; j < contentElems.length; j++) {
-        if (visibleElems.indexOf(j) === -1) {
-            var elem = contentElems[j];
+    for (var home_j = 0; home_j < contentElems.length; home_j++) {
+        if (visibleElems.indexOf(home_j) === -1) {
+            var elem = contentElems[home_j];
             elem.classList.add("hidden");
             elem.id = null;
         }
     }
 
-    i++;
+    home_i++;
 
-    if (i >= contentElems.length) {
-        i = 0;
+    if (home_i >= contentElems.length) {
+        home_i = 0;
     }
 }, 2500);
