802b4f98b6330408d7fc6ccd69c255797532cfb2
Vlad Zaharia <hey@vlad.gg>
2024-09-17 00:34:48 -0700

    Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), compressor()],
+  output: 'server',
+  adapter: cloudflare()
+});
