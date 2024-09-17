6a2d68ebfb092502c68947195344845ec94fc161
Vlad Zaharia <hey@vlad.gg>
2024-09-17 02:40:38 -0700

    Landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 import compressor from 'astro-compressor';
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor()],
+  integrations: [tailwind(), swup()],
   output: 'server',
   adapter: cloudflare()
 });
