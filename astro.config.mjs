7986014e76485e098ddd580bce252aefba4ca4b2
Vlad Zaharia <hey@vlad.gg>
2024-09-18 07:39:52 -0700

    Fonts and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,4 +26,9 @@ export default defineConfig({
       }
     }
   }),
+  vite: {
+    ssr: {
+      external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map((i) => `node:${i}`),
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				display: ['Rubik', ...defaultTheme.fontFamily.sans],
+				body: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans],
+				sans: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans],
+			},
 		},
 	},
 	plugins: [],
